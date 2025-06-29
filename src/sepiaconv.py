84ca6ca1fbb9f8e609b7400f346cdc2162056ebb
Gabriel R. <gabrielmio16@gmail.com>
2025-06-29 18:28:02 -0300

    break image into chunks

--- src/sepiaconv.py
+++ src/sepiaconv.py
@@ -5,35 +5,50 @@ import numpy as np
 def usage():
     print('Usage: sepiaconv.py <file>')
 
-def loadimage(filename):
+def load_image(filename):
     try:
         im = Image.open(filename)
     except:
         print('[ERROR] Could not load image.')
         exit(1)
 
-    return np.asarray(im,copy=True)
+    return im
 
-def pixeltosepia(rgb):
+def pixel_to_sepia(rgb):
     r,g,b    = rgb
     newRed   = min(int(0.393*r + 0.769*g + 0.189*b),255)
     newGreen = min(int(0.349*r + 0.686*g + 0.168*b),255)
     newBlue  = min(int(0.272*r + 0.534*g + 0.131*b),255)
     return [newRed, newGreen, newBlue]
 
-def imagetosepia(im):
+# Divide image into chunks of smaller size
+def chunkify(im, subdivs=2):
+    cells = (subdivs+1)**2
+    width = im.width//(subdivs+1)
+    height = im.height//(subdivs+1)
+    chunks = []
+
+    for i in range(cells):
+        left = (i%(subdivs+1))*width
+        top = (i//(subdivs+1))*height
+        right = left+width
+        bottom = top+height
+        chunks.append(im.crop((left,top,right,bottom)))
+
+    return chunks
+
+def image_to_sepia(im):
+    im = np.asarray(im,copy=True)
     h = len(im)
     w = len(im[0])
 
-    # TODO: multithreading
-
     for row in range(h):
         for col in range(w):
-            sep = pixeltosepia(im[row][col])
+            sep = pixel_to_sepia(im[row][col])
             im[row][col] = sep
 
-    newim = Image.fromarray(im, 'RGB')
-    return newim
+    im = Image.fromarray(im, 'RGB')
+    return im
 
 def main():
     argv = sys.argv
@@ -43,10 +58,25 @@ def main():
         usage()
         exit(1)
 
-    # TODO: accept multiple files at once
+    im = load_image(argv[1])
 
-    im = loadimage(argv[1])
-    newim = imagetosepia(im)
+    subdivs = 1
+    rows = subdivs+1
+    x,y = (0,0)
+
+    newim = Image.new("RGB", im.size)
+    chunks = chunkify(im, subdivs)
+
+    for i in range(len(chunks)):
+        print(i)
+        chunk = chunks[i]
+        sepia = image_to_sepia(chunk)
+        newim.paste(sepia, (x,y))
+
+        x += chunk.width
+        if (i+1)%rows == 0:
+            x = 0
+            y += chunk.height
 
     newim.save("out.jpg")
 
