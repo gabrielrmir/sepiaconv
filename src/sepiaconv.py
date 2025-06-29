26d6cfa9a54a5ec165982c8ed31e9ab20de3caeb
Gabriel R. <gabrielmio16@gmail.com>
2025-06-29 20:42:57 -0300

    formatting

--- src/sepiaconv.py
+++ src/sepiaconv.py
@@ -3,43 +3,48 @@ import threading
 from PIL import Image
 import numpy as np
 
+
 def usage():
-    print('Usage: sepiaconv.py <file>')
+    print("Usage: sepiaconv.py <file>")
+
 
 def load_image(filename):
     try:
         im = Image.open(filename)
     except:
-        print('[ERROR] Could not load image.')
+        print("[ERROR] Could not load image.")
         exit(1)
 
     return im
 
+
 def pixel_to_sepia(rgb):
-    r,g,b    = rgb
-    newRed   = min(int(0.393*r + 0.769*g + 0.189*b),255)
-    newGreen = min(int(0.349*r + 0.686*g + 0.168*b),255)
-    newBlue  = min(int(0.272*r + 0.534*g + 0.131*b),255)
+    r, g, b = rgb
+    newRed = min(int(0.393 * r + 0.769 * g + 0.189 * b), 255)
+    newGreen = min(int(0.349 * r + 0.686 * g + 0.168 * b), 255)
+    newBlue = min(int(0.272 * r + 0.534 * g + 0.131 * b), 255)
     return [newRed, newGreen, newBlue]
+
 
 # Divide image into chunks of smaller size
 def chunkify(im, subdivs):
-    cells = (subdivs+1)**2
-    width = im.width//(subdivs+1)
-    height = im.height//(subdivs+1)
+    cells = (subdivs + 1) ** 2
+    width = im.width // (subdivs + 1)
+    height = im.height // (subdivs + 1)
     chunks = []
 
     for i in range(cells):
-        left = (i%(subdivs+1))*width
-        top = (i//(subdivs+1))*height
-        right = left+width
-        bottom = top+height
-        chunks.append(im.crop((left,top,right,bottom)))
+        left = (i % (subdivs + 1)) * width
+        top = (i // (subdivs + 1)) * height
+        right = left + width
+        bottom = top + height
+        chunks.append(im.crop((left, top, right, bottom)))
 
     return chunks
 
+
 def image_to_sepia(im):
-    im = np.asarray(im,copy=True)
+    im = np.asarray(im, copy=True)
     h = len(im)
     w = len(im[0])
 
@@ -48,19 +53,22 @@ def image_to_sepia(im):
             sep = pixel_to_sepia(im[row][col])
             im[row][col] = sep
 
-    im = Image.fromarray(im, 'RGB')
+    im = Image.fromarray(im, "RGB")
     return im
+
 
 def chunk_processor(target_im):
     while True:
         with lock:
-            if len(data) == 0: break
+            if len(data) == 0:
+                break
             chunk, pos = data.pop()
 
         sepia = image_to_sepia(chunk)
 
         with lock:
             target_im.paste(sepia, pos)
+
 
 def main():
     argv = sys.argv
@@ -73,7 +81,7 @@ def main():
     im = load_image(argv[1])
 
     subdivs = 3
-    rows = subdivs+1
+    rows = subdivs + 1
     chunks = chunkify(im, subdivs)
 
     newim = Image.new("RGB", im.size)
@@ -83,13 +91,13 @@ def main():
         t = threading.Thread(target=chunk_processor, args=(newim,))
         threads.append(t)
 
-    x,y = (0,0)
+    x, y = (0, 0)
     for i in range(len(chunks)):
         chunk = chunks[i]
-        data.append((chunk.copy(), (x,y)))
+        data.append((chunk.copy(), (x, y)))
 
         x += chunk.width
-        if (i+1)%rows == 0:
+        if (i + 1) % rows == 0:
             x = 0
             y += chunk.height
 
@@ -101,8 +109,9 @@ def main():
 
     newim.save("out.jpg")
 
+
 lock = threading.Lock()
 data = []
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
